132d7387d558f3b7c619bf2421161f33c1d44963
Levon Becker <levon.becker.github@bonusbits.com>
2021-09-23 13:46:44 -0700

    tfstate bucket creation example script improved

--- files/environment_setup/s3_bucket_tfstate_demo.sh
+++ files/environment_setup/s3_bucket_tfstate_demo.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-aws cloudformation create-stack --stack-name demo-tfstate-bucket \
+stack_name="demo-s3-tfstate"
+bucket_name="bonusbits-demo-tfstate"
+owner_email="first.last@demo.com"
+iam_users_arns="arn:aws:iam::************:user/********"
+
+aws cloudformation create-stack --stack-name ${stack_name} \
  --profile $AWS_PROFILE \
  --region $AWS_REGION \
  --template-body file://$BB_ORCHESTRATOR_PATH/cloudformation/s3_bucket_tfstate.yml \
- --parameters ParameterKey='Owner',ParameterValue='first.last@demo.com' \
-              ParameterKey='Project',ParameterValue='Terraform Backend Storage' \
-              ParameterKey='DeleteAfter',ParameterValue='Never' \
-              ParameterKey='BucketName',ParameterValue='demo-tfstate' \
-              ParameterKey='IamUserList',ParameterValue='arn:aws:iam::************:user/********' \
+ --parameters ParameterKey='Owner',ParameterValue="${owner_email}" \
+              ParameterKey='Project',ParameterValue="Terraform Backend Storage" \
+              ParameterKey='DeleteAfter',ParameterValue="Never" \
+              ParameterKey='BucketName',ParameterValue="${bucket_name}" \
+              ParameterKey='IamUserList',ParameterValue="${iam_users_arns}" \
               ParameterKey='PreviousLifeCycle',ParameterValue='30' \
- --tags Key='Owner',Value='first.last@demo.com' \
+ --tags Key='Owner',Value="${owner_email}" \
         Key='Project',Value='Terraform Backend Storage' \
         Key='DeleteAfter',Value='Never'
 
@@ -18,7 +23,7 @@ read -n 1 -s -r -p "Wait for Stack Create to Finish then Press any key to contin
 echo ''
 
 aws s3api put-bucket-encryption \
-              --bucket demo-tfstate \
+              --bucket ${bucket_name} \
               --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
 
-aws s3api put-object --profile $AWS_PROFILE --region $AWS_REGION --bucket demo-tfstate --key workspaces/ --server-side-encryption AES256
+aws s3api put-object --profile $AWS_PROFILE --region $AWS_REGION --bucket ${bucket_name} --key workspaces/ --server-side-encryption AES256
